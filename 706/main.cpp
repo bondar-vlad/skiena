98185c4d4f2a098011f132728181f35cc1e8ba68
Vlad <bondar.vlad.1999@gmail.com>
2019-06-11 10:16:44 +0300

    #706: resolved

--- 706/main.cpp
+++ 706/main.cpp
@@ -6,24 +6,24 @@
 #include <cmath>
 #include <iomanip>
 
-#define RANGE(container) container.begin(), container.end()
+#define RANGE(container) container.begin(), container.end() - 1
 
 using namespace std;
 
 int n = 0;
 
-void draw_line_fragment(string s) {
+void draw_line_fragment(string s, bool end_space = true) {
     putchar(s[0]);
     for (int i = 0; i < n; ++i) {
         putchar(s[1]);
     }
     putchar(s[2]);
-    putchar(' ');
+    if (end_space) putchar(' ');
 }
 
 int main() {
     string s;
-    while (cin >> n >> s && n && s != "0") {
+    while (cin >> n >> s && n) {
         for_each(RANGE(s), [](const char &digit) {
             switch (digit) {
                 case '1':
@@ -35,6 +35,15 @@ int main() {
                     break;
             }
         });
+        switch (*s.rbegin()) {
+            case '1':
+            case '4':
+                draw_line_fragment("   ", false);
+                break;
+            default:
+                draw_line_fragment(" - ", false);
+                break;
+        }
         puts("");
         for (int i = 0; i < n; ++i) {
             for_each(RANGE(s), [](const char &digit) {
@@ -54,6 +63,21 @@ int main() {
                         break;
                 }
             });
+            switch (*s.rbegin()) {
+                case '1':
+                case '2':
+                case '3':
+                case '7':
+                    draw_line_fragment("  |", false);
+                    break;
+                case '5':
+                case '6':
+                    draw_line_fragment("|  ", false);
+                    break;
+                default:
+                    draw_line_fragment("| |", false);
+                    break;
+            }
             puts("");
         }
         for_each(RANGE(s), [](const char &digit) {
@@ -68,6 +92,16 @@ int main() {
                     break;
             }
         });
+        switch (*s.rbegin()) {
+            case '1':
+            case '7':
+            case '0':
+                draw_line_fragment("   ", false);
+                break;
+            default:
+                draw_line_fragment(" - ", false);
+                break;
+        }
         puts("");
         for (int i = 0; i < n; ++i) {
             for_each(RANGE(s), [](const char &digit) {
@@ -85,6 +119,19 @@ int main() {
                         break;
                 }
             });
+            switch (*s.rbegin()) {
+                case '2':
+                    draw_line_fragment("|  ", false);
+                    break;
+                case '6':
+                case '8':
+                case '0':
+                    draw_line_fragment("| |", false);
+                    break;
+                default:
+                    draw_line_fragment("  |", false);
+                    break;
+            }
             puts("");
         }
         for_each(RANGE(s), [](const char &digit) {
@@ -99,6 +146,16 @@ int main() {
                     break;
             }
         });
+        switch (*s.rbegin()) {
+            case '1':
+            case '4':
+            case '7':
+                draw_line_fragment("   ", false);
+                break;
+            default:
+                draw_line_fragment(" - ", false);
+                break;
+        }
         puts("");
         puts("");
     }
