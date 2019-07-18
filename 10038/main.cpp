48937131f2ac830637ee643ec3c5aef1c3b3b443
Vlad <bondar.vlad.1999@gmail.com>
2019-07-18 17:19:42 +0300

    #10038: clear project to start new (cause i had a big break)

--- 10038/main.cpp
+++ 10038/main.cpp
@@ -10,23 +10,6 @@
 using namespace std;
 
 int main() {
-    int n;
-    while (cin >> n) {
-        vector<int> v(n);
-        for_each(RANGE(v), [](int &i) { cin >> i; });
-        vector<bool> mask(n, false);
-        for (int j = 1; j < n; ++j) {
-            try {
-                mask.at(abs(v[j] - v[j - 1]) - 1) = true;
-            } catch (...) {
-                cout << "Not jolly\n";
-                break;
-            }
-        }
-        if (count(RANGE(mask), false))
-            cout << "Not jolly\n";
-        else
-            cout << "Jolly\n";
-    }
+
     return 0;
 }
